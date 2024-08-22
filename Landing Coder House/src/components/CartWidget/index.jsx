141682826eecd5f1106e692c3a6fabad223dd6f6
tomas-tissera <tomas.tissera.trabajo@gmail.com>
2024-08-22 18:43:03 -0300

    se agrego carrito icono y styles

--- Landing Coder House/src/components/CartWidget/index.jsx
+++ Landing Coder House/src/components/CartWidget/index.jsx
@@ -1,8 +1,11 @@
 import React from "react"
+import styles from './CartWidget.module.css'
+//Uso de React-Icon para llamar al icono de carrito
+import { CiShoppingCart } from "react-icons/ci";
 function CartWidget() {
   return (
     <>
-      <p>CartWidget</p>
+      <CiShoppingCart className={styles.carrito}/>
     </>
   )
 }
